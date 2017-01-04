0f208398f764be9f9134685eb6708b9ba266d71b
RobrechtS80 <r0383972@student.thomasmore.be>
2017-01-04 16:30:29 +0100

    validation sale finished

--- validate_Sale.js
+++ validate_Sale.js
@@ -1,21 +1,33 @@
 module.exports = {
     fieldsCorrect: function (object) {
         var errors=0;
-        if object[""]
+        if (object["saleid"]<=0||typeof object["saleeid"] !== "string"){
+            errors=errors+1;
+        };
         
+         if (object["product"]== ""||typeof object["product"] !== "string"){
+            errors=errors+1;
+        };
         
+         if (object["quantity"]<0||typeof object["quantity"] !== "number"){
+            errors=errors+1;
+        };
         
+         if (object["total"]<0||typeof object["total"] !== "number"){
+            errors=errors+1;
+        };
+         if (object["date"]==""||typeof object["date"] !== "string"){
+            errors=errors+1;
+        };
+         if (object["locationid"]==""||typeof object["locationid"] !== "string"){
+            errors=errors+1;
+        };
+        
+        return errors;//eerste keer vergete lol
         
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
+  
 };
 /*his.saleid = saleid;
     this.product = product;
